2513a4049477db5c4699d5bdb33aad0515d073e1
Dushyant Goel <dushyant95goel@gmail.com>
2025-10-02 21:09:09 +0100

    NLTK deployment issue

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 import nltk
 
 # Setup NLTK
-nltk.download()
-# nltk.download('punkt')
-# nltk.download('punkt_tab')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('averaged_preceptron_tagger_eng')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_preceptron_tagger_eng')
 
 # Define the pages
 main_page = st.Page('pages/main_page.py', title='🔎 Part-of-Speech Tagging and Named Entity Recognition')
